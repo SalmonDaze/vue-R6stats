439af05f5ce9532180639a3d357ce1dfe1934998
SalmonDaze <844344460@qq.com>
2018-05-24 15:22:01 +0800

    update

---
+++ src/components/about.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="about-container">
+        <nav-header page-title='关于' style='position:fixed;'></nav-header>
+        <div class='about'>
+            <h3>关于项目</h3>
+            <p>这是一个由<a href='http://apidocs.r6db.com/'>apidocs.r6db.com</a>提供API,基于Vue.js 2.0的项目</p>
+        </div>
+        <div class='about-hr'></div>
+        <div class='about2'>
+            <h3>关于作者</h3>
+            <p>作者:<a href='https://github.com/SalmonDaze'>SalmonDaze的github主页</a></p>
+            <p>项目源码:<a href='https://github.com/SalmonDaze/R6stats'>vue-r6stats</a></p>
+            <p>希望喜欢的朋友可以帮我点个star : )</p>
+        </div>
+        <div class='about-hr'></div>
+        <div class='about3'>
+            <h3>项目反馈</h3>
+            <p>反馈地址:<a href='https://github.com/SalmonDaze/R6stats/issues'>vue-r6stats/issues</a></p>
+        </div>
+        <div class='about-hr' style='margin-top:120px;'></div>
+    </div>
+</template>
+<script>
+import navHeader from './navHeader.vue'
+export default{
+    components:{
+        navHeader,
+    }
+}
+</script>
+<style>
+.about-container p{
+    margin-top:20px;
+}
+.about{
+    position:fixed;
+    top:80px;
+    left:30px;
+    font-size:0.9rem;
+}
+.about2{
+    position:fixed;
+    top:190px;
+    left:30px;
+    font-size:0.9rem;
+}
+.about3{
+    position:fixed;
+    top:360px;
+    left:30px;
+    font-size:0.9rem;
+}
+.about-container a{
+    text-decoration: none;
+    color:#409EFF;
+}
+.about-hr{
+    float:left;
+    width:90%;
+    background:grey;
+    height: 1px;
+    margin-top:170px;
+    margin-left:15px;
+    opacity: 0.3;
+}
+</style>
---
+++ src/components/menu.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class='menuContainer' :class="{'show':showMenus,'unshow':!showMenus}">
+        <span class='menu-back' @click='unshow'><i class='el-icon-circle-close-outline' style='margin-right:10px;font-size:20px'></i>返回</span>
+        <div class="menu-hr"></div>
+        <ul class='menu-list'>
+            <li @click='init'><router-link :to="{name:'index'}"><i class='el-icon-search menu-icon'></i>查询战绩</router-link></li>
+            <li><router-link :to="{name:'leaderboard'}"><i class='el-icon-upload2 menu-icon'></i>排行榜</router-link></li>
+            <li><router-link :to="{name:'favorite'}"><i class='el-icon-star-on menu-icon'></i>我的收藏</router-link></li>
+        </ul>
+        <div class="menu-hr menu-hr2"></div>
+        <ul class='menu-list'>
+            <li><router-link :to="{name:'about'}"><i class='el-icon-info menu-icon'></i>关于</router-link></li>
+        </ul>
+    </div>
+</template>
+<script>
+export default{
+    props:['showMenus'],
+    data(){
+        return{}
+    },
+    methods:{
+        unshow(){
+            this.$emit('unshow')
+        },
+        init(){
+            this.$store.dispatch('initData')
+        }
+    }
+}
+</script>
+<style >
+.menu-icon{
+    margin-right:20px;
+    font-weight: bold;
+}
+.menuContainer{
+    position:fixed;
+    width:200px;
+    height:100%;
+    background:white;
+    bottom:0px;
+    left:-200px;
+    z-index: 10;
+    box-shadow: 1px 0px 0px rgba(0,0,0,0.25);
+}
+.menu-list{
+    margin-left:-10px;
+    list-style-type: none;
+}
+.menu-list li{
+    margin-top:30px;
+    margin-left:40px;
+    font-weight: 600;
+}
+.menu-list li a{
+    text-decoration: none;
+    color:rgba(15,15,15,0.7);
+    font-size:0.9rem;
+}
+.show{
+    transition: all .3s ease;
+    transform: translateX(200px);
+}
+.unshow{
+    position:fixed;
+    left:-200px;
+    bottom:0px;
+    transition: all .3s ease;
+    transform: translateX(-200px);
+}
+.menu-back{
+    font-size:1.1rem;
+    position:absolute;
+    left:30px;
+    top:15px;
+}
+.menu-hr{
+    width:80%;
+    background:grey;
+    height: 1px;
+    margin-top:60px;
+    margin-left:15px;
+    opacity: 0.3;
+}
+.menu-hr2{
+    margin-top:20px;
+}
+</style>
