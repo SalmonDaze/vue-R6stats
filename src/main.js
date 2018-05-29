207dea22e047ee3219b20e8884e26031b5343e79
SalmonDaze <844344460@qq.com>
2018-05-29 15:51:49 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-
+import elementui from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-import elementui from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
 import store from './store/index'
 import storage from 'store'
 import VCharts from 'v-charts'
