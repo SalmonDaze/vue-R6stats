0884048df4bb31fe79b321c9633496627cdf79fc
SalmonDaze <844344460@qq.com>
2018-05-29 15:05:33 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 import elementui from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 import store from './store/index'
 import storage from 'store'
 import VCharts from 'v-charts'
